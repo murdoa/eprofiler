fb681233fc067a560e25458bb02f7d99dcda7670
murdoa <murdoa@proton.me>
2024-03-07 11:56:56 +0000

    Modified example to support first profiling

--- example/src/app.cpp
+++ example/src/app.cpp
@@ -1,15 +1,39 @@
 // #include <iostream>
 
-#include <eprofiler/eprofiler.hpp>
+#include <chrono>
+#include <thread>
 
+#include <spdlog/spdlog.h>
+
+#include <eprofiler/eprofiler.hpp>
 #include <eprofiler/linktimehashtable.hpp>
 
 using namespace eprofiler::literals;
 
 int app(int argc, char *argv[]) {
-    const auto profiler = eprofiler::EProfiler<"Profiler", int, int>{};
+    const auto profiler = eprofiler::EProfiler<"Profiler", std::size_t, std::chrono::steady_clock::time_point>{};
 
-    profiler["Test"_sc] = 5;
+    profiler["Inital_Tag"_sc] = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    profiler["Tag1"_sc] = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    profiler["Tag2"_sc] = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    profiler["Tag3"_sc] = std::chrono::steady_clock::now();
 
-    return profiler["Test"_sc];
+    spdlog::info("Profiler name: {}", profiler.name());
+    spdlog::info("Profiler offset: {}", profiler.get_offset());
+
+    spdlog::info("Inital_Tag: {}", profiler["Inital_Tag"_sc].time_since_epoch().count());
+    spdlog::info("Tag1: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag1"_sc] - profiler["Inital_Tag"_sc]).count());
+    spdlog::info("Tag2: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag2"_sc] - profiler["Inital_Tag"_sc]).count());
+    spdlog::info("Tag3: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag3"_sc] - profiler["Inital_Tag"_sc]).count());
+
+
+    // const auto profiler = eprofiler::EProfiler<"Profiler", int, int>{};
+
+    // profiler["Inital_Tag"_sc] = 1;
+    // return profiler["Inital_Tag"_sc];
+
+    return 0;
 }
