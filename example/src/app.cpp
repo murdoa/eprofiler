ec7ed2ba9fbcdb1e9c0f176092d18e86eb7a5f76
murdoa <murdoa@proton.me>
2024-03-08 02:24:06 +0000

    Updated example to minimal working example

--- example/src/app.cpp
+++ example/src/app.cpp
@@ -1,42 +1,23 @@
-// #include <iostream>
-
-#include <chrono>
-#include <thread>
-
-#include <spdlog/spdlog.h>
+#include <cstdlib>
 
 #include <eprofiler/eprofiler.hpp>
-#include <eprofiler/linktimehashtable.hpp>
 
 using namespace eprofiler::literals;
 
+struct SteadyClock {
+    using time_point = int;
+
+    static time_point now() {
+        return rand();
+    }
+};
+
 int app(int argc, char *argv[]) {
-    const auto profiler = eprofiler::EProfiler<"Profiler", std::size_t, std::chrono::steady_clock::time_point>{};
+    using EProfiler = eprofiler::EProfiler<eprofiler::EProfilerTag{"Test"}, int, SteadyClock>;
 
-    profiler["Inital_Tag"_sc] = std::chrono::steady_clock::now();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    EProfiler::set_time("Tag1"_sc);
+    EProfiler::set_time("Tag2"_sc);
+    EProfiler::set_time("Tag3"_sc);
 
-    profiler["Tag1"_sc] = std::chrono::steady_clock::now();
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
-    profiler["Tag2"_sc] = std::chrono::steady_clock::now();
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    
-    profiler["Tag3"_sc] = std::chrono::steady_clock::now();
-
-    spdlog::info("Profiler name: {}", profiler.name());
-    spdlog::info("Profiler offset: {}", profiler.get_offset());
-
-    spdlog::info("Inital_Tag: {}", profiler["Inital_Tag"_sc].time_since_epoch().count());
-    spdlog::info("Tag1: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag1"_sc] - profiler["Inital_Tag"_sc]).count());
-    spdlog::info("Tag2: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag2"_sc] - profiler["Inital_Tag"_sc]).count());
-    spdlog::info("Tag3: {}", std::chrono::duration_cast<std::chrono::microseconds>(profiler["Tag3"_sc] - profiler["Inital_Tag"_sc]).count());
-
-
-    // const auto profiler = eprofiler::EProfiler<"Profiler", int, int>{};
-
-    // profiler["Inital_Tag"_sc] = 1;
-    // return profiler["Inital_Tag"_sc];
-
-    return 0;
+    return EProfiler::get_duration("Tag1"_sc, "Tag3"_sc);
 }
